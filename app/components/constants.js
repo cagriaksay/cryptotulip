eafe4686a31013dbd9621612af15b8614de318b9
cagriaksay <cagriaksay@gmail.com>
2018-01-29 01:39:29 -0500

    Decrease gas price to 2 gwei

--- app/components/constants.js
+++ app/components/constants.js
@@ -1,5 +1,5 @@
 
-export const GAS_PRICE = '25000000000';
+export const GAS_PRICE = '2000000000';
 
 // export const CONTRACT_ADDRESS = '0x7b82756e5580d3d3ff6c139e7e8258b9a35d8918';
 // export const CONTRACT_ADDRESS = '0xcd65fb460141a56b913700000500c02d849d1632';
